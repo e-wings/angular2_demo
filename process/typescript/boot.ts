f2684cfd3b057830616f499e436871467684ddae
adam <adam.wuyu@gmail.com>
2016-04-29 16:25:01 +0800

    提交初始文件

---
+++ process/typescript/boot.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Welcome to my App 欢迎光临</h1>'
+})
+
+class AppComponent {
+
+}
+
+bootstrap(AppComponent);
