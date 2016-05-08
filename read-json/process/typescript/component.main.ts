45c3ff79cb2e509b40907323fddee689cf11cc09
adam <adam.wuyu@gmail.com>
2016-05-08 14:17:57 +0800

    可以在读取完成后执行其它逻辑了

--- read-json/process/typescript/component.main.ts
+++ read-json/process/typescript/component.main.ts
@@ -6,7 +6,7 @@ import 'rxjs/Rx';
   selector: 'parent-app',
   template: `
     <ul *ngIf="games">
-      <li *ngFor="#game of games.games">
+      <li *ngFor="#game of games">
         {{game.title}}
       </li>
     </ul>
@@ -14,13 +14,27 @@ import 'rxjs/Rx';
 })
 
 export class MainComponent {
-  games
+  public data;
+  public games;
   constructor(http: Http) {
     http.get('games.json')
       // Call map on the response observable to get the parsed games object
       .map(res => res.json())
       // Subscribe to the observable to get the parsed games object and attach it to the
       // component
-      .subscribe(games => this.games = games);
+      .subscribe(
+        data => this.jsonReady(data),
+        err => this.logError(err)
+      );
+  }
+  
+  jsonReady(data){
+    this.data=data;
+    this.games=data.games;
+    console.log("json ready");
+  }
+  
+  logError(err){
+    console.log(err);
   }
 }
