a20e1e422c2f50de352d0fafa28834e7929a9c47
qq1550966658 <1550966658@qq.com>
2016-05-10 14:17:02 +0800

    rc版项目搭建

---
+++ blank-project-rc/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1 class="title">welcome</h1>
+    <router-outlet></router-outlet>
+  `,
+})
+
+export class AppComponent {}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ blank-project-rc/app/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+
+//# sourceMappingURL=main.js.map
